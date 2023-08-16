1d41fcefa52f714b77c6298bcab3d13e7708979b
Elizabeth Yang <elizabethyang928@gmail.com>
2023-08-16 15:41:30 -0400

    Add Alert that a Choice is Already Taken

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const gridItem = document.querySelectorAll(".grid-item")
 const playerOne = document.querySelector("#player-one")
 const playerTwo = document.querySelector("#player-two")
 const players = [playerOne, playerTwo]
-const playerOneChoice = "X"
-const playerTwoChoice = "O"
+const playerXChoice = "X"
+const playerOChoice = "O"
 
 //CORE LOGIC
 let playerTurn = Math.floor(Math.random() * players.length) //randomly picks a player
@@ -28,34 +28,26 @@ button.addEventListener("click", () => {
 
 for (let i = 0; i <gridItem.length; i++) { //Iterates through grid items and performs the event listener
     gridItem[i].addEventListener("click", () => {
-        if (playerTurn === 0) {
-            gridItem[i].innerText = playerOneChoice
-            playerOne.classList.remove("player-turn") //removes playerOne box highlight
-            playerTwo.classList.add("player-turn") //adds playerTwo box highlight
-            playerTurn = 1 //switches playerTurn back to playerTwo
-            
+        if (gridItem[i].innerText === "X" || gridItem[i].innerText === "O") { //stops users from choosing a spot that is already taken
+            alert("Spot is already taken! Choose another move.")
         } else {
-            gridItem[i].innerText = playerTwoChoice
-            playerTwo.classList.remove("player-turn") //removes playerTwo box highlight
-            playerOne.classList.add("player-turn") //adds playerOne box highlight
-            playerTurn = 0 //switches playerTurn back to playerOne
+            if (playerTurn === 0) {
+                gridItem[i].innerText = playerXChoice //changes text to playerOne's choice
+                playerOne.classList.remove("player-turn") //removes playerOne box highlight
+                playerTwo.classList.add("player-turn") //adds playerTwo box highlight
+                playerTurn = 1 //switches playerTurn back to playerTwo
+            } else {
+                gridItem[i].innerText = playerOChoice //changes text to playerTwo's choice
+                playerTwo.classList.remove("player-turn") //removes playerTwo box highlight
+                playerOne.classList.add("player-turn") //adds playerOne box highlight
+                playerTurn = 0 //switches playerTurn back to playerOne
+            };
         }
-        
-        // gridItem[i].innerText = playerOneChoice //will change the inner text to "player choice" which we will assign later
-        // gridItem[i].innerText = playerTwoChoice
-    })
-}
-
-
-
+    });
+};
 
 /* 
 
--Turns must alternate between player 1 and 2 (highlight the box of whoever's turn it is)
-    define playerOne/Two as X or O, can give them a choice to choose if there's time
-    set conditions for if player one goes then it's player twos turn
-
-    if a players box is highlighted, the other player CANNOT GO
 -set win/lose conditions
 -set an alert for win or lose
 */
