93ceb48c52318128f5cc05d13e00a12216a4f3db
Elizabeth Yang <elizabethyang928@gmail.com>
2023-08-16 11:29:48 -0400

    Add tic tact toe grid and restart button event listener

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("test")
+const button = document.querySelector("button")
+button.addEventListener("click", (event) => {
+    location.reload() //this reloads the page but all user input will be lost
+})
